2f3573dc04f87e392293c9b8f7b35f88af622d68
ithalloelzo <ithalloelzo@gmail.com>
2026-01-16 15:36:07 -0300

    ✨ feat: criação do favorite repo

---
+++ src/repositories/favorite.repository.js
@@ -0,0 +1,41 @@
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+/*Cria o vínculo de favorito entre um usuário e um produto. */
+export const create = async (usuarioId, produtoId) => {
+  return await prisma.favorito.create({
+    data: {
+      usuarioId,
+      produtoId,
+    },
+  });
+};
+
+/*Busca uma relação específica para evitar duplicidade, utiliza a chave composta definida no schema*/
+export const findSpecific = async (usuarioId, produtoId) => {
+  return await prisma.favorito.findUnique({
+    where: {
+      usuarioId_produtoId: {
+        usuarioId,
+        produtoId,
+      },
+    },
+  });
+};
+
+/*Lista todos os favoritos de um usuário com os dados dos produtos. O 'include' permite retornar o payload enxuto solicitado na tarefa.*/
+export const findByUsuario = async (usuarioId) => {
+  return await prisma.favorito.findMany({
+    where: { usuarioId },
+    include: {
+      produto: true,
+    },
+  });
+};
+
+/* Remove um registro de favorito pelo ID*/
+export const remove = async (id) => {
+  return await prisma.favorito.delete({
+    where: { id },
+  });
+};
